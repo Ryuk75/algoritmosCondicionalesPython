1e15ca4030953996ec9e68e3748af15abddb244d
Ryuk75 <ricardonava75@outlook.es>
2021-09-13 04:29:25 -0500

    Ninth commit: ejercicio 8

--- Taller algoritmos condicionales.py
+++ Taller algoritmos condicionales.py
@@ -166,3 +166,38 @@ else:
     print("Ingrese un numero mayor que 0")
 print(f"El precio total a pagar por el estereo es: ${totalE}")
 print("")
+
+# 8.Una empresa quiere hacer una compra de varias piezas
+#de la misma clase a una fábrica de refacciones. La
+#empresa, dependiendo del monto total de la compra,
+#decidirá que hacer para pagar al fabricante. Si el monto
+#total de la compra excede de $500.000 la empresa tendrá
+#la capacidad de invertir de su propio dinero un 55% del
+#monto de la compra, pedir prestado al banco un 30% y el
+#resto lo pagará solicitando un crédito al fabricante. Si
+#el monto total de la compra no excede de $500.00 la
+#empresa tendrá capacidad de invertir de su propio dinero
+#un 70% y el restante 30% lo pagará solicitando crédito
+#al fabricante. El fabricante cobra por concepto de
+#interes un 20% sobre la cantidad que se le pague a
+#crédito. Obtener la cantidad a inverir, valor del
+#préstamo, valor del crédito y los intereses.
+print("8/ Obtener la cantidad a invertir, valor del préstamo, valor del crédito y los intereses.")
+cantidadI = int(input('Ingrese la cantidad a invertir: '))
+if cantidadI > 500000:
+    inversion = (cantidadI * 0.55)
+    prestamo = (cantidadI * 0.3)
+    prestamoFabrica = (cantidadI *0.15)
+    interes = (prestamoFabrica * 0.2)
+    print(f'La cantidad a invertir es ${inversion}')
+    print(f'La cantidad a prestamo al banco es ${prestamo}')
+    print(f'El prestamo a la fabrica es ${prestamoFabrica}')
+    print(f'Los intereses son: ${interes}')
+elif cantidadI <= 500000:
+    inversion = (cantidadI * 0.7)
+    prestamoFabrica = (cantidadI *0.3)
+    interes = (prestamoFabrica * 0.2)
+    print(f'La cantidad a invertir es ${inversion}')
+    print(f'El prestamo a la fabrica es ${prestamoFabrica}')
+    print(f'Los intereses son: ${interes}')
+print("")
