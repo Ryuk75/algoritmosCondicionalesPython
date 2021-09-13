35eed38a95209b84e5c1ccc965478ed06507e8e2
Ryuk75 <ricardonava75@outlook.es>
2021-09-13 04:43:46 -0500

    Eleventh commit: ejercicio 10

--- Taller algoritmos condicionales.py
+++ Taller algoritmos condicionales.py
@@ -216,3 +216,18 @@ elif (n1 < n2):
     resultado = n1 + n2
 print("El resultado es: ",resultado)
 print("")
+
+# 10. Leer tres números diferentes e imprimir el número
+#mayor de los tres
+print("10/ Lea tres numeros diferentes e imprima el mayor")
+n1 = int(input("Ingrese el primer numero: "))
+n2 = int(input("Ingrese el segundo numero: "))
+n3 = int(input("Ingrese el tercer numero: "))
+if (n1 > n3 and n1 > n2):
+    print(f"El numero mayor es el {n1}")
+elif (n2 > n3 and n2 > n1):
+    print(f"El numero mayor es el {n2}")
+elif (n3 > n2 and n3 > n1):
+    print(f"El numero mayor es el {n3}")
+else:
+    print("Todos los numeros son iguales.")
