353edb45e82a9fc4c184b48f6aeedb328f4ad5ea
Ryuk75 <ricardonava75@outlook.es>
2021-09-12 19:28:42 -0500

    third commit: ejercicio 2

--- Taller algoritmos condicionales.py
+++ Taller algoritmos condicionales.py
@@ -21,3 +21,22 @@ if (cantCamisas >= 3):
 elif (cantCamisas < 3):
     total = (cantCamisas*precioCamisas)
 print("El total a pagar es: ",total)
+print("")
+
+# 2. En un supermercado se hace una promoción, mediante la
+#cual el cliente obtiene un descuento dependiendo de un
+#número que se escoge al azar. Si el número escogido es menor
+#que 74 el descuento es del 15% sobre el total de la compra,
+#si es mayor o igual a 74 el descuento es del 20%. Obtener
+#cuanto dinero se le descuenta.
+print("2/ Cuanto dinero se le descuenta a un cliente")
+totalCompra = int(input("Ingrese el monto total de la compra: "))
+numeroAzar = int(input("Ingrese el numero escogido al azar: "))
+descuentoA = 0.15
+descuentoB = 0.2
+if (numeroAzar < 74):
+    descuentoTotal = totalCompra * descuentoA
+elif (numeroAzar >= 74):
+    descuentoTotal = totalCompra * descuentoB
+print("El descuento total de la compra es: ",descuentoTotal)
+print("")
